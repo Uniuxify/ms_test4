007f217b16e100659ef47b5753f55912b182b4e3
Uniuxify <Uniuxify@gmail.com>
2023-12-27 07:37:55 +0300

    changed lib for requests

--- init_script.py
+++ init_script.py
@@ -1,18 +1,19 @@
-import requests
+from urllib.request import urlopen
 base_url = 'https://raw.githubusercontent.com/Uniuxify/ms_test4/main/'
-response = requests.get(base_url + 'init.py')
-init = response.text.split('\n\n')
-response = requests.get(base_url + 'mu1.py')
-mu1 = response.text.split('\n\n')
-response = requests.get(base_url + 'mu2.py')
-mu2 = response.text.split('\n\n')
-response = requests.get(base_url + 'sigma1.py')
-sigma1 = response.text.split('\n\n')
-response = requests.get(base_url + 'sigma2.py')
-sigma2 = response.text.split('\n\n')
-response = requests.get(base_url + 'mumu.py')
-mumu = response.text.split('\n\n')
-response = requests.get(base_url + 'mumumu.py')
-mumumu = response.text.split('\n\n')
+with urlopen(base_url + 'init.py') as response:
+	init = response.read().decode().split('\n\n')
+with urlopen(base_url + 'mu1.py') as response:
+	mu1 = response.read().decode().split('\n\n')
+with urlopen(base_url + 'mu2.py') as response:
+	mu2 = response.read().decode().split('\n\n')
+with urlopen(base_url + 'sigma1.py') as response:
+	sigma1 = response.read().decode().split('\n\n')
+with urlopen(base_url + 'sigma2.py') as response:
+	sigma2 = response.read().decode().split('\n\n')
+with urlopen(base_url + 'mumu.py') as response:
+	mumu = response.read().decode().split('\n\n')
+with urlopen(base_url + 'mumumu.py') as response:
+	mumumu = response.read().decode().split('\n\n')
 del response
 del base_url
+del urlopen
